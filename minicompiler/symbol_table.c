df2182f59857db8fef13af6bb9753942b02cb17c
Erik Danielsson <danielsson.erik.0@gmail.com>
2020-07-30 23:59:38 +0200

    Adapted for typing purposes

--- minicompiler/symbol_table.c
+++ minicompiler/symbol_table.c
@@ -4,17 +4,28 @@
 #include "symbol_table.h"
 #include "hashing.h"
 
-struct SymTab* create_SymTab(int table_size)
+struct SymTab* create_SymTab(int table_size, struct SymTab* parent)
 {
     struct SymTab* symbol_table = malloc(sizeof(struct SymTab));
-    symbol_table->table_size  = table_size;
+    symbol_table->table_size = table_size;
     symbol_table->entries = malloc(sizeof(struct SymTab_entry*) * table_size);
 
     for (int i = 0; i < table_size; i++) {
         symbol_table->entries[i] = NULL;
     }
-
+    symbol_table->parent = parent;
+    symbol_table->n_childs = 0;
     return symbol_table;
+}
+
+void SymTab_append_child(struct SymTab* parent, struct SymTab* child)
+{
+    struct SymTab** tmp;
+    memcpy(tmp, parent_childs, parent->n_childs);
+    int n_childs = ++parent->n_childs;
+    parent->childs = malloc(sizeof(struct SymTab*)*n_childs);
+    memcpy(tmp, parent_childs, n_childs-1);
+    parent->childs[n_childs-1] = child;
 }
 
 struct SymTab_entry* SymTab_pair(char* key,
@@ -29,7 +40,7 @@ struct SymTab_entry* SymTab_pair(char* key,
     return entry;
 }
 
-void SymTab_set(struct SymTab* symbol_table, char* key,
+int SymTab_check_and_set(struct SymTab* symbol_table, char* key,
                     enum SymbolType type, void* symbol)
 {
     unsigned int slot = hash(key, symbol_table->table_size);
@@ -37,34 +48,66 @@ void SymTab_set(struct SymTab* symbol_table, char* key,
     struct SymTab_entry* prev;
     if (entry == NULL) {
         symbol_table->entries[slot] = SymTab_pair(key, type, symbol);
-        return;
+        return 0;
     }
     while (entry != NULL) {
-        if (strcmp(entry->key, key) == 0) {
-            entry->type = type;
-            return;
+
+        if (entry->type == type && strcmp(entry->key, key) == 0) {
+            printf("Equal\n");
+            return 1;
         }
         prev = entry;
         entry = prev->next;
     }
     prev->next = SymTab_pair(key, type, symbol);
+    return 0;
 }
 
-enum SymbolType SymTab_get(struct SymTab* symbol_table, char* key,
-                            void** symbol_out)
+enum SymbolType find_in_table(struct SymTab* symbol_table, unsigned int hashv, char* key,
+                                    void** symbol_out)
 {
-    unsigned int slot = hash(key, symbol_table->table_size);
-    struct SymTab_entry* entry = symbol_table->entries[slot];
-
+    struct SymTab_entry* entry = symbol_table->entries[hashv];
     while (entry != NULL) {
         if (strcmp(entry->key, key) == 0) {
-            return entry->type;
             *symbol_out = entry->symbol;
+            return entry->type;
+
         }
         entry = entry->next;
     }
     return -1;
 }
+
+/*
+ * Search symbol table and parents recursivly for symbol
+ */
+enum SymbolType SymTab_getr(struct SymTab* symbol_table, char* key,
+                            void **symbol_out)
+{
+    unsigned int hashv = hash(key, symbol_table->table_size);
+    enum SymbolType type;
+    for(; symbol_table != NULL; symbol_table = symbol_table->parent) {
+        if ((type = find_in_table(symbol_table,  hashv, key, symbol_out)) != -1)
+            return type;
+    }
+    (*symbol_out) = NULL;
+    return -1;
+}
+
+int SymTab_type_declared(struct SymTab* symbol_table, char* key)
+{
+    unsigned int slot = hash(key, symbol_table->table_size);
+    for(; symbol_table != NULL; symbol_table = symbol_table->parent) {
+        struct SymTab_entry* entry = symbol_table->entries[slot];
+        while (entry != NULL) {
+            if (entry->type == STRUCTURE && strcmp(entry->key, key) == 0)
+                return 1;
+            entry = entry->next;
+        }
+    }
+    return 0;
+}
+
 
 void SymTab_destroy(struct SymTab* symbol_table)
 {
@@ -93,10 +136,10 @@ void SymTab_destroy(struct SymTab* symbol_table)
     symbol_table = NULL;
 }
 
-void SymTab_dump(struct SymTab* symbol_table)
+void SymTab_dump(struct SymTab* symbol_table, char* title)
 {
     printf("\n");
-    printf("Symbols:\n");
+    printf("%s:\n", title);
     for (int i = 0; i < symbol_table->table_size; i++) {
         struct SymTab_entry* entry = symbol_table->entries[i];
         if (entry == NULL) {
--- minicompiler/symbol_table.h
+++ minicompiler/symbol_table.h
@@ -4,7 +4,6 @@ enum SymbolType {
     VARIABLE,
     STRUCTURE,
     FUNCTION,
-    TYPE,
 };
 
 struct SymTab_entry {
@@ -18,20 +17,26 @@ struct SymTab {
     int table_size;
     struct SymTab_entry** entries;
     struct SymTab* parent;
+    int n_childs;
+    struct SymTab** childs;
 };
 
-struct SymTab* create_SymTab();
+struct SymTab* create_SymTab(int table_size, struct SymTab* parent);
+
+void SymTab_append_child(struct SymTab* parent, struct SymTab* child),
 
 struct SymTab_entry* SymTab_pair(char* key,
                                 enum SymbolType type,
                                 void* symbol);
 
-void SymTab_set(struct SymTab* symboltable, char* key,
+int SymTab_check_and_set(struct SymTab* symboltable, char* key,
                     enum SymbolType type, void* symbol);
 
 enum SymbolType SymTab_get(struct SymTab* symboltable, char* key,
                             void** symbol_out);
 
+int SymTab_type_declared(struct SymTab* symbol_table, char* type_names);
+
 void SymTab_destroy(struct SymTab* symboltable);
 
-void SymTab_dump(struct SymTab* symboltable);
+void SymTab_dump(struct SymTab* symboltable, char* title);
