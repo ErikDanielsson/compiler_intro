30650be904cccd4eaa208f515d7e206e6d18409a
Erik Danielsson <danielsson.erik.0@gmail.com>
2020-08-02 01:53:01 +0200

    Hash func for general matching

--- minicompiler/hashing.c
+++ minicompiler/hashing.c
@@ -12,3 +12,17 @@ unsigned int hash(const char* key, int table_size)
     }
     return h % table_size;
 }
+
+unsigned int max_hash(const char* key)
+{
+    // FVN (Fowler / Noll / Vo) hash function:
+    const char *p = key;
+    unsigned int key_len = strlen(key);
+    unsigned int h = 0x811c9dc5;
+    int i;
+
+    for ( i = 0; i < key_len; i++ ) {
+        h = ( h ^ p[i] ) * 0x01000193;
+    }
+    return h;
+}
--- minicompiler/hashing.h
+++ minicompiler/hashing.h
@@ -1,1 +1,2 @@
 unsigned int hash(const char* key, int table_size);
+unsigned int max_hash(const char* key);
