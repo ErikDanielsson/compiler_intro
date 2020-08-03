95b8b08d80e737cdcae503b94c53d1dc867ec62c
Erik Danielsson <danielsson.erik.0@gmail.com>
2020-08-03 02:05:59 +0200

    This was weird code

--- minicompiler/hashing.c
+++ minicompiler/hashing.c
@@ -2,13 +2,9 @@
 unsigned int hash(const char* key, int table_size)
 {
     // FVN (Fowler / Noll / Vo) hash function:
-    const char *p = key;
-    unsigned int key_len = strlen(key);
     unsigned int h = 0x811c9dc5;
-    int i;
-
-    for ( i = 0; i < key_len; i++ ) {
-        h = ( h ^ p[i] ) * 0x01000193;
+    for (int i = 0; i < strlen(key); i++ ) {
+        h = ( h ^ key[i] ) * 0x01000193;
     }
     return h % table_size;
 }
@@ -16,13 +12,9 @@ unsigned int hash(const char* key, int table_size)
 unsigned int max_hash(const char* key)
 {
     // FVN (Fowler / Noll / Vo) hash function:
-    const char *p = key;
-    unsigned int key_len = strlen(key);
     unsigned int h = 0x811c9dc5;
-    int i;
-
-    for ( i = 0; i < key_len; i++ ) {
-        h = ( h ^ p[i] ) * 0x01000193;
+    for (int i = 0; i < strlen(key); i++ ) {
+        h = ( h ^ key[i] ) * 0x01000193;
     }
     return h;
 }
